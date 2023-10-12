d46ddf7cbb99f2aa207b72625be75bbb18d8176f
michael mobile <mobile.dev@vn.farm-angel.com>
2023-10-12 17:35:48 +0700

    ui

---
+++ lib/screens/list_staff/views/list_staff_dialog.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rp_checkin/base/base_screen.dart';
+
+class ListStaffDialog extends StatefulWidget {
+  const ListStaffDialog({super.key});
+
+  @override
+  State<ListStaffDialog> createState() => _ListStaffDialogState();
+}
+
+class _ListStaffDialogState extends State<ListStaffDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return BaseScreen(
+      body: Container(),
+    );
+  }
+}
